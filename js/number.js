1d9b36d6626c098621db5e9912fe2ae3dedd7f44
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-06-08 10:38:35 +0300

    initial commit

---
+++ js/number.js
@@ -0,0 +1,23 @@
+const a = 0;
+const b = 3.14;
+const c = -7;
+const d = Infinity;
+const e = -Infinity;
+const f = NaN //Not A Number
+
+console.log(7 + 5);
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.abs(7));
+console.log(Math.ceil(2.000001));
+
+console.log(Math.floor(2.87));
+
+console.log(Math.round(2.5));
+console.log(Math.round(2.4));
+
+console.log(Math.pow(2, 6));
+console.log(Math.max(1, 135, 9192)); //grazina didziausia skaiciu
+console.log(Math.min(1, 55, 2314512)); //grazina maziausia skaiciu
+
+console.log(Math.random());
