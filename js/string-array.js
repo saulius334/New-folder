ea32eefb3f29edb033a7381e0dc377190608577c
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-06-14 12:36:58 +0300

    split, join

---
+++ js/string-array.js
@@ -0,0 +1,30 @@
+const str = `labas rytas lietuva`;
+const words = str.split(` `);
+const wordcount = words.length;
+console.log(words);
+console.log(wordcount);
+
+const str2 = `Vasara`;
+const words2 = str2.split(``)
+console.log(words2);
+
+
+const person = `vardenis pavardenis`;
+const inicialai = person
+    .split(` `)
+    .map(s => s[0])
+    .join(`.`) + `.`;
+console.log(inicialai);
+
+//same as below
+
+
+let inicialai2 = person[0];
+for (let i = 1; i < person.length; i++) {
+    const letter = person[i];
+    if (person[i-1] === ` `) {
+        inicialai2 += `.` + letter;
+    }
+}
+inicialai2 += `.`
+console.log(inicialai2);
