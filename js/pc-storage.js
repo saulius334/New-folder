10bad69466e8129f627881f2a98a5e5764a31f14
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-06-22 10:52:56 +0300

    pc file search nd

--- js/pc-storage.js
+++ js/pc-storage.js
@@ -7,9 +7,31 @@ const pc = {
 
 }
 
-function storage() {
-    return 0;
+function storage(item, depth = 0) {
+    let size = 0;
+
+    if (item.type === `folder`) {
+        for (const innerItem of item.content) {
+            size += storage(innerItem, depth +1);
+        }
+    } else {
+        size = item.size;
+    }
+
+    return depth ? size : size / 8;
 }
 
 const totalKBs = storage(pc);
-console.log(`Used space:`, totalKBs);
+console.log(`Used space:`, totalKBs, `KB`);
+
+const files = [
+
+]
+
+function fileSearch(input) {
+    return {
+        folder: [],
+        files: [],
+    };
+
+}
