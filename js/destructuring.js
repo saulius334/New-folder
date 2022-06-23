e10ed97fc74b7c05a35893233ed1f46ae2a02bda
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-06-23 12:02:25 +0300

    operator func

--- js/destructuring.js
+++ js/destructuring.js
@@ -22,6 +22,10 @@ const person = {
 
 const {age, children, ...otherinfo} = person;
 const [bart, liza] = children
+const {age: BartAge} = bart
+const {age: LizaAge, name: Lizaname} = liza
+console.log(`liza age:`, LizaAge, `Liza name:`, Lizaname);
+console.log(`bart age:`, BartAge);
 console.log(age);
 console.log(bart);
 console.log(liza);
@@ -40,3 +44,29 @@ console.log(firster);
 console.log(second);
 console.log(five);
 console.log(other);
+
+
+function info(...params) {
+  return `info: ${params.length}`;
+}
+console.log(info());
+console.log(info(1, 2, true, undefined, NaN, `lol`));
+console.log(info(1, NaN, `lol`));
+console.log(info( undefined, NaN, 1, `lol`));
+console.log(info(1, 2, true, undefined, NaN, 2, `lol`));
+console.log();
+console.clear();
+console.log(`----------------`);
+function biggest(...list) {
+return Math.max(...list)
+}
+console.log(biggest(2, 4, -2, -10, 9, 10));
+
+
+function asd(firstname, lastname, ...other) {
+  console.log(firstname);
+  console.log(lastname);
+  console.log(other);
+  return `?`
+}
+console.log(asd(`saul`, `good`, `man`, 123, true));
